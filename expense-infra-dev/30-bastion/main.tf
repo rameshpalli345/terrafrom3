405931def73b91c32442b5990ffb16424e4350f6
ramesh <rameshpalli@hotmail.com>
2024-09-30 00:22:25 +0200

    expense infra

---
+++ expense-infra-dev/30-bastion/main.tf
@@ -0,0 +1,19 @@
+module "bastion" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  ami = data.aws_ami.joindevops.id
+  name = local.resource_name
+
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id              = local.public_subnet_ids
+
+  tags = merge (
+    var.common_tags,
+    var.bastion_tags,
+
+    {
+      Name = local.resource_name
+  }
+  )
+}
